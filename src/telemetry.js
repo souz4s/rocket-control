bdeb9c93e8e10f77e4d18b598fe118af15dcdb08
souz4s <psouza.1650@gmail.com>
2021-11-17 23:55:05 -0300

    removing telemetry time interval

--- src/telemetry.js
+++ src/telemetry.js
@@ -9,27 +9,25 @@ const telemetry = async() => {
         }
     })
 
-    setInterval(function() {
-        try {
-            let vessel = client.send(spaceCenter.getActiveVessel())
-            let control = vessel.control.get();
-            let orbitalReference = vessel.orbitalReferenceFrame.get();
-            let flight = vessel.flight(orbitalReference);
-            //setting data for telemetry
-            let getThrottleCall = spaceCenter.controlGetThrottle(control.id)
-            let getHeadingCall = spaceCenter.flightGetHeading(flight.id)
-            let getElevationCall = spaceCenter.flightGetElevation(flight.id)
-            let response = client.send([getThrottleCall, getHeadingCall, getElevationCall])
-            console.log({
-                throttle: response.results[0].value,
-                heading: response.results[1].value,
-                elevation: response.results[2].value
-            })
-        } catch (err) {
-            client.close()
-            err
-        }
-    }, 1000)
+    try {
+        let vessel = await client.send(spaceCenter.getActiveVessel())
+        let control = await vessel.control.get();
+        let orbitalReference = await vessel.orbitalReferenceFrame.get();
+        let flight = await vessel.flight(orbitalReference);
+        //setting data for telemetry
+        let getThrottleCall = spaceCenter.controlGetThrottle(control.id)
+        let getHeadingCall = spaceCenter.flightGetHeading(flight.id)
+        let getElevationCall = spaceCenter.flightGetElevation(flight.id)
+        let response = await client.send([getThrottleCall, getHeadingCall, getElevationCall])
+        console.log({
+            throttle: response.results[0].value,
+            heading: response.results[1].value,
+            elevation: response.results[2].value
+        })
+    } catch (err) {
+        await client.close()
+        throw err
+    }
 }
 
 
