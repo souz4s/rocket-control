7e5a76380480c98bb1d30d4a1b148453f7bc9257
souz4s <psouza.1650@gmail.com>
2022-05-30 21:45:56 -0300

    next.js and typescript configuration

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const telemetry = require('./telemetry') //importing telemetry data
-const connect = require('./connection/connect') //importing connection data
-
-const run = async() => { //commands sent to the rocket
-    await connect() //report connection data
-        //await telemetry() //report telemetry data
-}
-
-run()
