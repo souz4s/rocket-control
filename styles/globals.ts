72bbfd014c6f2ff36a53a8f7bd283a6712b2e2cd
souz4s <psouza.1650@gmail.com>
2022-05-30 22:27:31 -0300

    styled-components setup

---
+++ styles/globals.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    html,
+    body {
+        margin: 0;
+	    padding: 0;
+	    border: 0;
+	    vertical-align: baseline;
+        list-style: none;
+    }
+`;
