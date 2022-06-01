fcd97a502e48efe3f14524b82d7215d0bdbd0898
souz4s <psouza.1650@gmail.com>
2022-05-31 23:26:01 -0300

    used connection API

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,6 +1,15 @@
 import Head from "next/head";
+import axios from "axios";
 
 const Home: React.FC = () => {
+  const connect = async () => {
+    const response = await axios.get(`http://localhost:8888/api/controller/connect`);
+
+    return response;
+  }
+
+  const init = connect();
+
   return (
     <>
       <Head>
