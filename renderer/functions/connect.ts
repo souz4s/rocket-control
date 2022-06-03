e787c52686672440a50970fee4478a60f32025be
souz4s <p.uzah@outlook.com>
2022-06-03 16:24:28 -0300

    removal of comments

--- renderer/functions/connect.ts
+++ renderer/functions/connect.ts
@@ -3,10 +3,9 @@ import createClient from "krpc-node";
 const connect = async () => {
   const client = await createClient({
     rpc: {
-      // default values for connection with krpc
-      protocol: "ws", // protocol used is WebSockets
+      protocol: "ws",
       host: "127.0.0.1",
-      port: "50000/?name=connect", // setting port and client name
+      port: "50000/?name=connect",
     },
   });
 };
