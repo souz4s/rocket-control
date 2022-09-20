4daf89adb6eceab366018828dce6f90c46bb4fae
souz4s <psouza.1650@gmail.com>
2022-09-20 00:22:18 -0300

    test: added parameter in spy class to get active vessel

--- tests/data/mocks/mock-krpc-vessel.ts
+++ tests/data/mocks/mock-krpc-vessel.ts
@@ -2,8 +2,10 @@ import { GetActiveVesselRepository } from "@/data/protocols";
 import { mockVesselParams } from "@/tests/domain/mocks";
 
 export class GetActiveVesselRepositorySpy implements GetActiveVesselRepository {
+  params: GetActiveVesselRepository.Params | undefined;
   result = mockVesselParams();
-  getActiveVessel = async (): Promise<GetActiveVesselRepository.Result> => {
+  getActiveVessel = async (params: GetActiveVesselRepository.Params): Promise<GetActiveVesselRepository.Result> => {
+    this.params = params;
     return { vessel: this.result.vessel };
   };
 }
