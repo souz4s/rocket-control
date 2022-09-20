d3b0a82c88a5d8b0f09c96f11a56373ce473ad86
souz4s <psouza.1650@gmail.com>
2022-09-20 00:31:42 -0300

    feat: getting active vessel and returning in krpc connection log

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,8 @@
 /* eslint-disable */
-import { krpcClient } from "@/infrastructure";
+import { krpcClient } from "@/infrastructure/krpc";
+import { VesselRepository } from "@/infrastructure/krpc/repositories";
 
-krpcClient.createConnection().then(() => {
-  console.log("Successfully connected");
+krpcClient.createConnection().then(async (response) => {
+  const vesselRepository = new VesselRepository().getActiveVessel(response);
+  console.log(`Connected to ${(await vesselRepository).vessel}`);
 });
