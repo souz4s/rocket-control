0995651ec2e4199090fed5ac27faa54b5de59ac5
souz4s <psouza.1650@gmail.com>
2022-09-20 00:10:41 -0300

    test: client parameter added in application use case test to get active vessel

--- tests/data/use-cases/krpc-get-active-vessel.spec.ts
+++ tests/data/use-cases/krpc-get-active-vessel.spec.ts
@@ -2,6 +2,8 @@ import { KrpcGetActiveVessel } from "@/data/use-cases";
 import { mockVesselParams } from "@/tests/domain/mocks";
 import { GetActiveVesselRepositorySpy } from "@/tests/data/mocks";
 
+import { faker } from "@faker-js/faker";
+
 const makeSut = () => {
   const getActiveVesselRepositorySpy = new GetActiveVesselRepositorySpy();
   const sut = new KrpcGetActiveVessel(getActiveVesselRepositorySpy);
@@ -12,7 +14,7 @@ describe("KrpcGetActiveVessel", () => {
   it("should return the 'vessel'", async () => {
     const { sut, getActiveVesselRepositorySpy } = makeSut();
     getActiveVesselRepositorySpy.result = mockVesselParams();
-    const result = await sut.perform();
+    const result = await sut.perform({ client: JSON.parse(faker.datatype.json()) });
     expect(result).toHaveProperty("vessel");
     expect(result.vessel).toBe(getActiveVesselRepositorySpy.result.vessel);
   });
