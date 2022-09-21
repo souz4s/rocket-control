c1dfe0de03890233762a5d008f4508f6edbc054b
souz4s <p.uzah@outlook.com>
2022-09-21 10:08:21 -0300

    fix: .catch added for catching error

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,12 @@
-/* eslint-disable */
 import { krpcClient } from "@/infrastructure/krpc";
 import { VesselRepository } from "@/infrastructure/krpc/repositories";
 
-krpcClient.createConnection().then(async (response) => {
-  const vesselRepository = new VesselRepository().getActiveVessel(response);
-  console.log(`Connected to ${(await vesselRepository).vessel}`);
-});
+krpcClient
+  .createConnection()
+  .then(async (response) => {
+    const vesselRepository = new VesselRepository().getActiveVessel(response);
+    console.log(`Connected to ${(await vesselRepository).vessel}`);
+  })
+  .catch((err) => {
+    console.log(err instanceof Error ? err.message : err);
+  });
