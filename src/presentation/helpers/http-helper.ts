8306bf102fac13f2bc058fc3e4de2bcd65f3e477
souz4s <psouza.1650@gmail.com>
2022-10-04 23:06:44 -0300

    feat: bad request http helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -2,6 +2,11 @@ import { InternalServerError } from "@/presentation/erros";
 import { HttpResponse } from "@/presentation/protocols";
 
 export class HttpHelper {
+  static BAD_REQUEST = (err: Error): HttpResponse<Error> => ({
+    statusCode: 400,
+    body: err,
+  });
+
   static INTERNAL_SERVER_ERROR = (err: Error): HttpResponse<Error> => ({
     statusCode: 500,
     body: new InternalServerError(err.stack || ""),
