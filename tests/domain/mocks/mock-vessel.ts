90f1dae39f5f0f818c87ce29a59fe72fb5f39c66
souz4s <p.uzah@outlook.com>
2022-11-29 21:10:33 -0300

    perf: toString removed from vessel faker params

--- tests/domain/mocks/mock-vessel.ts
+++ tests/domain/mocks/mock-vessel.ts
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
 
 export const mockVesselParams = () => ({
-  vessel: faker.internet.userName().toString(),
+  vessel: faker.internet.userName(),
   biome: faker.datatype.string(),
 });
