9847d76dccdc5b70d33c9102ccc94dca6a7c6507
souz4s <p.uzah@outlook.com>
2022-12-04 16:06:57 -0300

    feat: vessel model

---
+++ src/domain/models/vessel.ts
@@ -0,0 +1,13 @@
+export type VesselModel = {
+  altitude: number;
+  apoapsis: number;
+  atmosphereDensity: number;
+  dryMass: number;
+  latitude: number;
+  longitude: number;
+  periapsis: number;
+  pitch: number;
+  signalStrength: number;
+  speed: number;
+  vesselMass: number;
+};
