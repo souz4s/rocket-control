d1d41368e991c7f6dbc74492714e727d1ac7c38e
souz4s <p.uzah@outlook.com>
2022-12-04 15:39:13 -0300

    perf: obsolete files removed

--- src/domain/use-cases/get-active-vessel.ts
+++
@@ -1,8 +0,0 @@
-export interface GetActiveVessel {
-  perform: (params: GetActiveVessel.Params) => Promise<GetActiveVessel.Result>;
-}
-
-export namespace GetActiveVessel {
-  export type Params = { [client: string]: any };
-  export type Result = { vessel: string };
-}
--- tests/domain/mocks/mock-vessel.ts
+++
@@ -1,5 +0,0 @@
-import { faker } from "@faker-js/faker";
-
-export const mockVesselParams = () => ({
-  vessel: faker.internet.userName(),
-});
