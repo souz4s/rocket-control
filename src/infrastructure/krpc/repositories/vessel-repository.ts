262a4b991a7e8c953005530bdee13912350b778b
souz4s <p.uzah@outlook.com>
2022-12-06 02:37:57 -0300

    feat: capture of vessel parameters

--- src/infrastructure/krpc/repositories/vessel-repository.ts
+++ src/infrastructure/krpc/repositories/vessel-repository.ts
@@ -1,4 +1,4 @@
-import { GetActiveVesselRepository } from "@/data/protocols/krpc";
+import { GetActiveVesselRepository, GetVesselInfoRepository } from "@/data/protocols/krpc";
 import { krpcClient } from "@/infrastructure/krpc";
 
 export class VesselRepository implements GetActiveVesselRepository {
@@ -6,5 +6,40 @@ export class VesselRepository implements GetActiveVesselRepository {
     const vesselSelection = await params.send(krpcClient.getConnection().getActiveVessel());
     const vessel = await vesselSelection.name.get();
     return { vessel: vessel };
+  };
+
+  getVesselInfo = async (params: GetVesselInfoRepository.Params): Promise<GetVesselInfoRepository.Result> => {
+    const vesselSelection = await params.send(krpcClient.getConnection().getActiveVessel());
+    const vesselOrbit = await vesselSelection.orbit.get();
+    const vesselFlight = await vesselSelection.flight(vesselSelection);
+    const vesselComms = await vesselSelection.comms.get();
+    const altitude = await vesselFlight.meanAltitude.get();
+    const apoapsis = await vesselOrbit.apoapsisAltitude.get();
+    const atmosphereDensity = await vesselFlight.atmosphereDensity.get();
+    const dryMass = await vesselSelection.dryMass.get();
+    const gForce = await vesselFlight.gForce.get();
+    const latitude = await vesselFlight.latitude.get();
+    const longitude = await vesselFlight.longitude.get();
+    const periapsis = await vesselOrbit.periapsisAltitude.get();
+    const pitch = await vesselFlight.pitch.get();
+    const signalStrength = await vesselComms.signalStrength.get();
+    const speed = await vesselFlight.speed.get();
+    const vesselMass = await vesselSelection.mass.get();
+    return {
+      vessel: {
+        altitude: altitude,
+        apoapsis: apoapsis,
+        atmosphereDensity: atmosphereDensity,
+        dryMass: dryMass,
+        gForce: gForce,
+        latitude: latitude,
+        longitude: longitude,
+        periapsis: periapsis,
+        pitch: pitch,
+        signalStrength: signalStrength,
+        speed: speed,
+        vesselMass: vesselMass,
+      },
+    };
   };
 }
